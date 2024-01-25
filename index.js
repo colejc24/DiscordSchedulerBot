de8607bf4ff9e34681ac0435509befde1ba24521
Cole <colejc24@gmail.com>
2024-01-24 20:27:06 -0600

    idk what I did this time. kinda added slash commands but they don't work?

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import dotenv from 'dotenv'
-dotenv.config()     
-
-import { Client, Events, GatewayIntentBits } from 'discord.js';
-
 const fs = require('node:fs');
 const path = require('node:path');
+const de = require('dotenv');
+const { Client, Events, Collection, GatewayIntentBits } = require('discord.js');
+
+de.config()     
+
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
@@ -15,7 +15,29 @@ const client = new Client({
 
 client.commands = new Collection();
 
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        if ('data' in command && 'execute' in command) {
+            client.commands.set(command.data.name, command);
+        } else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+    }
+}
+
 client.login(process.env.DISCORD_TOKEN);
+
+client.on(Events.InteractionCreate, interaction => {
+    if (!interaction.isChatInputCommand()) return;
+    console.log(interaction);
+});
 
 client.on("messageCreate", async (message) => {
     
